fefac874b3c900ba5c364fda9b726c7ce4018ca0
irfankhan309 <irfanqehs@gmail.com>
2020-09-22 19:02:18 +0530

    intial commit

---
+++ Voyage_Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\voyage_holidays_proj\voyage_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Voyage_Proj/Voyage_LandingApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VoyageLandingappConfig(AppConfig):
+    name = 'Voyage_LandingApp'
