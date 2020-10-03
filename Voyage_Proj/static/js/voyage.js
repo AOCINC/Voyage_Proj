e753845e953648531f121ebde143daa11a46e5e5
irfankhan309 <irfanqehs@gmail.com>
2020-10-03 10:32:34 +0530

    till date hide & show feature updated

---
+++ Voyage_Proj/static/js/voyage.js
@@ -0,0 +1,9 @@
+// hiding/showing calender for till data 
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
